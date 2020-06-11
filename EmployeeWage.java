4469a4f3d3bec381577f38fd3350998de8f92b6f
Rajeshnalla21 <nallarajesh21@gmail.com>
2020-06-11 13:05:21 +0530

    Employee Wage Java Program

---
+++ EmployeeWage.java
@@ -0,0 +1,35 @@
+public class EmployeeWage {
+	//Constans
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+
+	public static int computeEmpWage(int empRatePerHours, int numOfWorkingDays, int maxHoursPerMonth) {
+		//Variables
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+      //Computation
+      System.out.println("Welcome to Employee Wage Computation");
+      while (totalEmpHrs <=  maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+         totalWorkingDays++;
+         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+         switch(empCheck) {
+            case IS_PART_TIME:
+               empHrs = 4;
+               break;
+            case IS_FULL_TIME:
+               empHrs = 8;
+               break;
+            default:
+               empHrs = 0;
+         }
+         totalEmpHrs += empHrs;
+         System.out.println("Day#:" + totalWorkingDays + " Employee Hours: " + empHrs);
+      }
+      int totalEmpWage = totalEmpHrs * empRatePerHours;
+      System.out.println("Total Employee Wage for Company is: "+ totalEmpWage);
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args) {
+		computeEmpWage( 20, 20, 100);
+	}
+}

dfe402081a11833dc52b98fb0e4f20660b49d6ab
Rajeshnalla21 <nallarajesh21@gmail.com>
2020-06-13 07:20:24 +0530

    Third Use case employee wage computation in java

---
+++ EmpWageUc3.java
@@ -0,0 +1,19 @@
+public class EmpWageUc3 {
+	public static void main(String[] args) {
+		int IS_PART_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int EMP_RATE_PER_HR = 20;
+		int empHrs = 0;
+		int empWage = 0;
+		System.out.println("Welcome to Employee Wage Computation");
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_PART_TIME)
+			empHrs = 8;
+		else if (empCheck == IS_FULL_TIME)
+         empHrs = 12;
+		else
+			empHrs = 0;
+		empWage = empHrs * EMP_RATE_PER_HR;
+		System.out.println("Employee Wage: " + empWage);
+	}
+}

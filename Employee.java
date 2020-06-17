568006000ba3431f2a9b31b196a77be6d4a98455
Rajeshnalla21 <65641402+Rajeshnalla21@users.noreply.github.com>
2020-06-17 08:22:28 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,55 +1,56 @@
+public class EmpWageBuilderObject {
+	//Constans
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
 
-class Employee{
-    // class constants
-    static final int DAYSLIMIT = 20;
-    static final int HRSLIMIT = 100;
+	private final String company;
+	private final int empRatePerHours;
+	private final int numOfWorkingDays;
+	private final int maxHoursPerMonth;
+	private int totalEmpWage;
 
-    // object constants
-    final int partTimeHrs = 4;
-    final int fullTimeHrs = 8;
-    final int absent = 0;
+	public EmpWageBuilderObject(String company, int empRatePerHours, int numOfWorkingDays, int maxHoursPerMonth) {
+		this.company = company;
+		this.empRatePerHours = empRatePerHours;
+		this.numOfWorkingDays = numOfWorkingDays;
+		this.maxHoursPerMonth = maxHoursPerMonth;
+	}
 
-    // object variable
-    final int wagePerHrs;
+	public void computeEmpWage() {
+		//Variables
+		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+      //Computation
+      System.out.println("Welcome to Employee Wage Computation");
+      while (totalEmpHrs <=  maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+         totalWorkingDays++;
+         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+         switch(empCheck) {
+            case IS_PART_TIME:
+               empHrs = 8;
+               break;
+            case IS_FULL_TIME:
+               empHrs = 12;
+               break;
+            default:
+               empHrs = 0;
+         }
+         totalEmpHrs += empHrs;
+         System.out.println("Day#: " + totalWorkingDays + "Employee Hours: " + empHrs);
+      }
+      totalEmpWage = totalEmpHrs * empRatePerHours;
+   }
 
-    // variables
-    int dailyWage = 0;
-    int monthlyWage = 0;
-    int monthlyhrs = 0;
-    int counter = 1;
+	@Override
+	public String toString() {
+		return "Total Employee Wage for Company:" +company+" is: "+ totalEmpWage;
+	}
 
-    // constructor
-    Employee(String company, int wagePerHrs){
-        System.out.println("Company name: "+company+"\nWage per hour: "+wagePerHrs);
-        this.wagePerHrs = wagePerHrs;
-        calculateMonthlyWage();
-    }
-
-    void calculateMonthlyWage(){
-        while(counter <= DAYSLIMIT && monthlyhrs <= HRSLIMIT){
-            switch(Helper.isPresent(3)){
-                case 0:
-                    dailyWage = Helper.dailyWage(fullTimeHrs, wagePerHrs);
-                    monthlyhrs += fullTimeHrs;
-                    break;
-                case 1:
-                    dailyWage = Helper.dailyWage(absent, wagePerHrs);
-                    monthlyhrs += absent;
-                    break;
-                case 2:
-                    dailyWage = Helper.dailyWage(partTimeHrs, wagePerHrs);
-                    monthlyhrs += partTimeHrs;
-                    break;
-            }
-            monthlyWage += dailyWage;
-            counter++;
-        }
-        System.out.println("Monthly wage: "+ monthlyWage+"\nMonthly hrs: "+monthlyhrs+"\nDays :"+counter);
-    }
-
-    public static void main(String[] args){
-        System.out.println("Welcome");
-        Employee BridgeLabz = new Employee("BridgeLabz", 20);
-        Employee Google = new Employee("Google", 30);
-    }
+	public static void main(String[] args) {
+		EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 20, 100);
+		EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 50);
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
+	}
 }
